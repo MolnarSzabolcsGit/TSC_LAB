ae515cc2cbff700cb86691482f8f3d06d91d3400
root <root@DESKTOP-PNV0UVL.localdomain>
2021-03-10 17:48:59 +0200

    Laboratorul 3

--- lab3/lab02_clocking_blocks/top.sv
+++ lab3/lab02_clocking_blocks/top.sv
@@ -1,5 +1,10 @@
 /***********************************************************************
  * A SystemVerilog top-level netlist to connect testbench to DUT
+ *
+ * SystemVerilog Training Workshop.
+ * Copyright 2006, 2013 by Sutherland HDL, Inc.
+ * Tualatin, Oregon, USA.  All rights reserved.
+ * www.sutherland-hdl.com
  **********************************************************************/
 
 module top;
@@ -11,34 +16,24 @@ module top;
   // clock variables
   logic clk;
   logic test_clk;
-  
-  // interconnecting signals
-  tb_ifc intf(.clk(test_clk));
 
-  // instantiate testbench and connect ports
-  instr_register_test test (
-    .clk(test_clk),
-    .load_en(intf.load_en),
-    .reset_n(intf.reset_n),
-    .operand_a(intf.operand_a),
-    .operand_b(intf.operand_b),
-    .opcode(intf.opcode),
-    .write_pointer(intf.write_pointer),
-    .read_pointer(intf.read_pointer),
-    .instruction_word(intf.instruction_word)
-   );
+ // instantiate the testbench interface
+  tb_ifc io (.clk(test_clk));
 
-  // instantiate design and connect ports
+  // connect interface to testbench
+  instr_register_test test (.io(io));
+
+  // connect interface to design using discrete ports
   instr_register dut (
     .clk(clk),
-    .load_en(intf.load_en),
-    .reset_n(intf.reset_n),
-    .operand_a(intf.operand_a),
-    .operand_b(intf.operand_b),
-    .opcode(intf.opcode),
-    .write_pointer(intf.write_pointer),
-    .read_pointer(intf.read_pointer),
-    .instruction_word(intf.instruction_word)
+    .load_en(io.load_en),
+    .reset_n(io.reset_n),
+    .operand_a(io.operand_a),
+    .operand_b(io.operand_b),
+    .opcode(io.opcode),
+    .write_pointer(io.write_pointer),
+    .read_pointer(io.read_pointer),
+    .instruction_word(io.instruction_word)
    );
 
   // clock oscillators
@@ -51,6 +46,9 @@ module top;
     test_clk <=0;
     // offset test_clk edges from clk to prevent races between
     // the testbench and the design
+    //
+    // THIS TEST CLOCK WILL BE REPLACED BY A CLOCKING BLOCK IN THE
+    // INTERFACE BETWEEN THE TESTBENCH AND THE DUT
     #4 forever begin
       #2ns test_clk = 1'b1;
       #8ns test_clk = 1'b0;
